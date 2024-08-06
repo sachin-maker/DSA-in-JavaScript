1c5ea464499f9e1c6e7a73879f2ad99f50187006
sachin-maker <sachindeshpande456@gmail.com>
2024-08-07 00:41:48 +0530

    Pattern1 added

---
+++ StartPattern/Pattern1.js
@@ -0,0 +1,44 @@
+// type 1
+// *
+// **
+// ***
+// ****
+// *****
+
+
+
+// Number of rows for the pattern
+const rows = 5;
+
+// Loop to iterate through each row
+for (let i = 1; i <= rows; i++) {
+  let pattern = '';
+  
+  // Loop to add '*' characters in each row
+  for (let j = 1; j <= i; j++) {
+    pattern += '*';
+  }
+  
+  // Print the current row pattern
+  console.log(pattern);
+}
+
+// ==========================================================================================
+
+// type 2
+// *****
+// ****
+// ***
+// **
+// *
+
+
+for(let i=1;i<=5;i++){
+    let pattern=''
+    for(let j=5;j>=i;j--){
+        pattern +='*'
+        
+    }
+    console.log(pattern)
+    
+}
