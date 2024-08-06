f4ac3230f097f80b807d86d0a1bd1704da7e05ef
sachin-maker <sachindeshpande456@gmail.com>
2024-08-06 15:44:10 +0530

    charcter count in string

---
+++ JS_Coding/13_Occurence_string.js
@@ -0,0 +1,24 @@
+
+//  Given a string, write a function to count the occurrences of each character in the string. 
+
+
+
+function countCharacterOccurrences(str) {
+    const characterCounts = {};
+
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+        if (characterCounts[char]) {
+            characterCounts[char]++;
+        } else {
+            characterCounts[char] = 1;
+        }
+    }
+
+    return characterCounts;
+}
+
+// Example usage
+const exampleString = "hello world";
+const result = countCharacterOccurrences(exampleString);
+console.log(result);
