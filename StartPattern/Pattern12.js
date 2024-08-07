08fac0c97e4aa215d2e4d15b233e25493dc941de
sachin-maker <sachindeshpande456@gmail.com>
2024-08-08 00:40:25 +0530

    pattern 13 added

---
+++ StartPattern/Pattern12.js
@@ -0,0 +1,23 @@
+
+//Type 13
+// *   *
+//  * * 
+//   *  
+//  * * 
+// *   *
+
+
+
+for(let i=0;i<5;i++){
+    let pattern =''
+    for(let j=0;j<5;j++){
+        if(i==j || i+j ==4){
+            pattern +='*'
+        }
+        else{
+            pattern +=' '
+        }
+        
+    }
+    console.log(pattern)
+}
