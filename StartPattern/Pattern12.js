e1abc4f8ecd72267c7b2ad989d6450b406ed97b2
sachin-maker <sachindeshpande456@gmail.com>
2024-08-08 00:47:46 +0530

    issued fixed

--- StartPattern/Pattern12.js
+++ StartPattern/Pattern12.js
@@ -8,9 +8,9 @@
 
 
 
-for(let i=1;i<=5;i++){
+for(let i=0;i<5;i++){
     let pattern =''
-    for(let j=1;j<=5;j++){
+    for(let j=0;j<5;j++){
         if(i==j || i+j ==4){
             pattern +='*'
         }
