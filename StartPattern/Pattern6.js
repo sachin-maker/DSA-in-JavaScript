7da12e8885e9f5b1dbacefca94172f9940e12d90
sachin-maker <sachindeshpande456@gmail.com>
2024-08-07 15:39:20 +0530

    pattern 7 added

---
+++ StartPattern/Pattern6.js
@@ -0,0 +1,17 @@
+// type 7
+//      *
+//     * *
+//    * * *
+//   * * * *
+//  * * * * *
+
+for(let i=1;i<=5;i++){
+let  pattern =''
+for(let j=4;j>=i;j--){
+   pattern +=' '
+}
+for(let k=1;k<=i;k++){
+   pattern +=' *'
+}
+console.log(pattern)
+}
